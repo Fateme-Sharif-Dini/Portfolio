96eef90d28fa61772ab48a4b1e9ae29ddb0fbf97
Fateme-Sharif-Dini <fgsharifdini@gmail.com>
2025-03-23 20:53:28 +0330

    feat(app): added login
    added login page
    
    BREAKING CHANGE: nothing

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,5 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = { extends: ['@commitlint/config-conventional'], 
+  rules: {
+    'header-min-length': [2, 'always', 10],
+  }
+ };
